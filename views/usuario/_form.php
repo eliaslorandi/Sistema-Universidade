9d65eefadcbbd7866384f33e695fe5afc8f77e87
Elias Lorandi <elias.lorandi@hotmail.com>
2023-07-24 10:41:44 -0300

    Criação usuario controller

---
+++ views/usuario/_form.php
@@ -0,0 +1,29 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
+/** @var yii\web\View $this */
+/** @var app\models\Usuario $model */
+/** @var yii\widgets\ActiveForm $form */
+?>
+
+<div class="usuario-form">
+
+    <?php $form = ActiveForm::begin(); ?>
+
+    <?= $form->field($model, 'LOGIN')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'SENHA')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'NOME')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'NUCLEO_ID')->textInput() ?>
+
+    <div class="form-group">
+        <?= Html::submitButton('Save', ['class' => 'btn btn-success']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
---
+++ views/usuario/update.php
@@ -0,0 +1,21 @@
+<?php
+
+use yii\helpers\Html;
+
+/** @var yii\web\View $this */
+/** @var app\models\Usuario $model */
+
+$this->title = 'Update Usuario: ' . $model->ID;
+$this->params['breadcrumbs'][] = ['label' => 'Usuarios', 'url' => ['index']];
+$this->params['breadcrumbs'][] = ['label' => $model->ID, 'url' => ['view', 'ID' => $model->ID]];
+$this->params['breadcrumbs'][] = 'Update';
+?>
+<div class="usuario-update">
+
+    <h1><?= Html::encode($this->title) ?></h1>
+
+    <?= $this->render('_form', [
+        'model' => $model,
+    ]) ?>
+
+</div>
