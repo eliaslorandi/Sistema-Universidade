c6544528915ebd2b723e7de1314a5f6ba7bd2739
Elias Lorandi <elias.lorandi@hotmail.com>
2023-07-24 10:41:09 -0300

    Criação oferta controller

---
+++ views/oferta/_form.php
@@ -0,0 +1,29 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
+/** @var yii\web\View $this */
+/** @var app\models\Oferta $model */
+/** @var yii\widgets\ActiveForm $form */
+?>
+
+<div class="oferta-form">
+
+    <?php $form = ActiveForm::begin(); ?>
+
+    <?= $form->field($model, 'SEMESTRE_INICIO')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'MATRIZ_ID')->textInput() ?>
+
+    <?= $form->field($model, 'USUARIO_ID')->textInput() ?>
+
+    <?= $form->field($model, 'DATA_REGISTRO')->textInput(['maxlength' => true]) ?>
+
+    <div class="form-group">
+        <?= Html::submitButton('Save', ['class' => 'btn btn-success']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
---
+++ views/oferta/index.php
@@ -0,0 +1,47 @@
+<?php
+
+use app\models\Oferta;
+use yii\helpers\Html;
+use yii\helpers\Url;
+use yii\grid\ActionColumn;
+use yii\grid\GridView;
+
+/** @var yii\web\View $this */
+/** @var app\models\OfertaSearch $searchModel */
+/** @var yii\data\ActiveDataProvider $dataProvider */
+
+$this->title = 'Ofertas';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="oferta-index">
+
+    <h1><?= Html::encode($this->title) ?></h1>
+
+    <p>
+        <?= Html::a('Create Oferta', ['create'], ['class' => 'btn btn-success']) ?>
+    </p>
+
+    <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
+
+    <?= GridView::widget([
+        'dataProvider' => $dataProvider,
+        'filterModel' => $searchModel,
+        'columns' => [
+            ['class' => 'yii\grid\SerialColumn'],
+
+            'ID',
+            'SEMESTRE_INICIO',
+            'MATRIZ_ID',
+            'USUARIO_ID',
+            'DATA_REGISTRO',
+            [
+                'class' => ActionColumn::className(),
+                'urlCreator' => function ($action, Oferta $model, $key, $index, $column) {
+                    return Url::toRoute([$action, 'ID' => $model->ID]);
+                 }
+            ],
+        ],
+    ]); ?>
+
+
+</div>
