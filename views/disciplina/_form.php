d6da3996d40cb34f171dfb5df023a4da4b578033
Elias Lorandi <elias.lorandi@hotmail.com>
2023-07-27 20:56:06 -0300

    Att

--- views/disciplina/_form.php
+++ views/disciplina/_form.php
@@ -1,7 +1,11 @@
 <?php
 
 use yii\helpers\Html;
+use yii\helpers\Arrayhelper;
 use yii\widgets\ActiveForm;
+
+use app\models\Nucleo;
+use app\models\Matriz;
 
 /** @var yii\web\View $this */
 /** @var app\models\Disciplina $model */
@@ -14,13 +18,23 @@ use yii\widgets\ActiveForm;
 
     <?= $form->field($model, 'NOME')->textInput(['maxlength' => true]) ?>
 
-    <?= $form->field($model, 'CH')->textInput() ?>
+    <?= $form->field($model, 'CH')->textInput(['type' => 'number']) ?>
 
-    <?= $form->field($model, 'PERIODO')->textInput() ?>
+    <?= $form->field($model, 'PERIODO')->textInput(['type' => 'number']) ?>
 
-    <?= $form->field($model, 'NUCLEO_ID')->textInput() ?>
+    <?= $form->field($model, 'NUCLEO_ID')
+        ->dropDownList(
+            ArrayHelper::map(Nucleo::find()->orderBy('NOME')->all(), 'ID', 'NOME'),
+            ['prompt' => 'Selecione um Núcleo']
+        )
+    ?>
 
-    <?= $form->field($model, 'MATRIZ_ID')->textInput() ?>
+    <?= $form->field($model, 'MATRIZ_ID')
+        ->dropDownList(
+            ArrayHelper::map(Matriz::find()->orderBy('SIGLA')->all(), 'ID', 'SIGLA'),
+            ['prompt' => 'Selecione uma Matriz']
+        )
+    ?>
 
     <div class="form-group">
         <?= Html::submitButton('Save', ['class' => 'btn btn-success']) ?>
