0bdd5404d48207e9f2c00481c345b8570670efeb
Elias Lorandi <elias.lorandi@hotmail.com>
2023-09-19 14:47:09 -0300

    Att

--- views/site/index.php
+++ views/site/index.php
@@ -33,7 +33,8 @@ $this->title = 'Sistema Universidade';
                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
                     dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
                     ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
+                    fugiat nulla pariatur.
+                </p>
 
                 <p><a class="btn btn-outline-secondary" href="https://www.yiiframework.com/forum/">Yii Forum &raquo;</a></p>
             </div>
